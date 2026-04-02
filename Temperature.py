73fe5bdbc90ec2235cee37e787c87436f9cdd0d8
JoelLara <jlara38@my.gcu.edu>
2026-04-02 14:12:47 -0700

    Joel Lara

--- Temperature.py
+++ Temperature.py
@@ -1,3 +1,10 @@
-class Temp:
-    def Temperature(self):
-        print("temp")
+class Temperature:
+    def __init__(self, value, time):
+        self.value = value
+        self.timestamp = time
+
+    def get_temperature(self):
+        return self.value
+    
+    def get_timestamp(self):
+        return self.timestamp
